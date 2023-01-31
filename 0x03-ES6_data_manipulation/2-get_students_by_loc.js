7cffca534e90efc5935fe8b49364db1b0d7c8a19
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-01-31 22:56:47 +0100

    go

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,6 @@
 export default function getStudentsByLocation(studentArray, city){
 
-   return studentArray.filter((value) => {
-            if(value.location == city){
-                return value;
-            } 
+   return studentArray.filter((value) => {    
+        return (value.location == city) ? value : "";
     });
 }
---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(studentArray){
+    return studentArray.reduce((total, value, index, array) => {
+        return total + umbervalue.id;
+    });
+}
