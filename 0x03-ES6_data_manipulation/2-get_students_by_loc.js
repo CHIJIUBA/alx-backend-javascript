7584e5bc22a3b68219523649a898195075271062
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-01-31 23:29:04 +0100

    go

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
 export default function getStudentsByLocation(studentArray, city){
-
-   return studentArray.filter((value) => {    
-        return (value.location == city) ? value : "";
-    });
+   return studentArray.filter((value) => value.location == city);
 }
