da6eec06b91f1fdc9ececd138c59030c01f0a0c1
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-01-31 19:05:35 +0100

    go

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,15 @@
-export default function getListStudentIds(){
-    
+export default function getListStudentIds(studentArray){
+    let studentId = new Array();
+
+    if(studentArray instanceof Array){
+
+        studentArray.map((value, index, array) => {
+            studentId.push(array[index].id);
+        });
+        
+        return studentId;
+    }
+    else{
+        return studentId;
+    }
 }
